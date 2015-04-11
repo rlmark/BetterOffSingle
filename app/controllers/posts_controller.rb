80cae494b7423364ca131d74b0503dde93afeb85
Rebecca Mark <rebecca.lmark@gmail.com>
2015-04-11 15:57:30 -0700

    added basic form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,15 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def update
@@ -15,8 +22,15 @@ class PostsController < ApplicationController
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
   end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:text)
+    end
 end
