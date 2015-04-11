bdde5c818dc06cb392d5f0cbdb5646a50137d64e
Rebecca Mark <rebecca.lmark@gmail.com>
2015-04-11 15:10:24 -0700

    added posts views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/update'
+
+  get 'posts/edit'
+
+  get 'posts/destroy'
+
+  get 'posts/index'
+
+  get 'posts/show'
+
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
